e1f3ad10d3d2e2855087d48307010894bd7a59f0
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-21 22:40:06 +0530

    [Shweta] Modified The two lines of equality is checked

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -6,7 +6,7 @@ class Program
         {
             Console.WriteLine("WelCome to Line Comparison Computation Program");
             //Console.ReadLine();
-            double x1, x2, y1, y2, result;
+            double x1, x2, y1, y2, result1;
             Console.WriteLine("Enter the x and y coordinate of first point { ");
              x1=Convert.ToDouble(Console.ReadLine());
              y1 = Convert.ToDouble(Console.ReadLine());
@@ -17,8 +17,25 @@ class Program
             double x = (x2 - x1) * (x2 - x1);
             double y=(y2-y1)*(y2-y1);
             double sum = x + y;
-            result = Math.Sqrt(sum);
-            Console.WriteLine("Length of line is {0}", result);
+            result1 = Math.Sqrt(sum);
+            Console.WriteLine("Length of first line is {0}", result1);
+
+            double a1, a2, b1, b2, result2;
+            Console.WriteLine("Enter the x and y coordinate of first point { ");
+            a1 = Convert.ToDouble(Console.ReadLine());
+            b1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the x and y coordinate of second point { ");
+            a2 = Convert.ToDouble(Console.ReadLine());
+            b2 = Convert.ToDouble(Console.ReadLine());
+
+            double a = (a2 - a1) * (a2 - a1);
+            double b = (b2 - b1) * (b2 - b1);
+            double sum1 = a + b;
+            result2 = Math.Sqrt(sum1);
+            Console.WriteLine("Length of Second line is {0}", result2);
+
+            bool Compare = result1.Equals(result2);
+            Console.WriteLine("Lines are equal:-{0}", Compare);
 
         }
     }
