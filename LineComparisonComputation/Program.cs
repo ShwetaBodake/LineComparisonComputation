bfc1ab31b62537a205d9e8343a4a692423258050
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-21 22:17:49 +0530

    [Shweta] Modified Length of line is calculated

--- LineComparisonComputation/Program.cs
+++ LineComparisonComputation/Program.cs
@@ -5,7 +5,21 @@ class Program
         public static void Main(string[] args)
         {
             Console.WriteLine("WelCome to Line Comparison Computation Program");
-            Console.ReadLine();
+            //Console.ReadLine();
+            double x1, x2, y1, y2, result;
+            Console.WriteLine("Enter the x and y coordinate of first point { ");
+             x1=Convert.ToDouble(Console.ReadLine());
+             y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the x and y coordinate of second point { ");
+             x2 = Convert.ToDouble(Console.ReadLine());
+             y2 = Convert.ToDouble(Console.ReadLine());
+
+            double x = (x2 - x1) * (x2 - x1);
+            double y=(y2-y1)*(y2-y1);
+            double sum = x + y;
+            result = Math.Sqrt(sum);
+            Console.WriteLine("Length of line is {0}", result);
+
         }
     }
 }
